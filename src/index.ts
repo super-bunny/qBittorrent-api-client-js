9fefc8b7713c7de98cc8de87008e7a37d7b5347c
Clovis de Villars <clovis.devillars@gmail.com>
2022-10-09 19:12:53 +0200

    Export all types from package for end users

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
+// Client exports
 export { default } from './classes/QBittorrentClient'
 export { default as QBittorrentClient } from './classes/QBittorrentClient'
 export * from './classes/QBittorrentClient'
 
+// Client Error exports
 export { default as QBittorrentClientError } from './utils/QBittorrentClientError'
 export * from './utils/QBittorrentClientError'
+
+// Types exports
+export * from './types/QBittorrentAppMethods'
+export * from './types/QBittorrentSyncMethods'
+export * from './types/QBittorrentTorrentsMethods'
+export * from './types/QBittorrentTransferMethods'
